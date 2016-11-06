d5a33eb8e5b23b0cc83ff1b66720aa50b12b18f6
djeang <djeangdev@yahoo.fr>
2016-11-06 23:17:23 +0100

    add methods

--- org.jerkar.eclipse-plugin/src/org/jerkar/eclipseplugin/menu/LaunchMenu.java
+++ org.jerkar.eclipse-plugin/src/org/jerkar/eclipseplugin/menu/LaunchMenu.java
@@ -1,15 +1,17 @@
 package org.jerkar.eclipseplugin.menu;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.Flags;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
@@ -18,7 +20,9 @@ import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.ITypeHierarchy;
+import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
 import org.eclipse.jface.action.ContributionItem;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
@@ -46,26 +50,33 @@ public class LaunchMenu extends ContributionItem {
         // You can also simply return if you do not want to show a menu
 
         IProject project = project();
-        if (project instanceof IJavaProject) {
-
+        if (isJavaProject(project)) {
+            IJavaProject javaProject = JavaCore.create(project);
+            try {
+                fill(menu, index, javaProject);
+            } catch (JavaModelException e) {
+                throw new RuntimeException(e);
+            }
         }
 
-        // create the menu item
-        MenuItem menuItem = new MenuItem(menu, SWT.CHECK, index);
-        menuItem.setText("My menu item (" + new Date() + ")");
-        menuItem.addSelectionListener(new SelectionAdapter() {
-            public void widgetSelected(SelectionEvent e) {
-                // what to do when menu is subsequently selected.
-                System.err.println("Dynamic menu selected");
-            }
-        });
     }
 
     private static void fill(Menu menu, int index, IJavaProject javaProject) throws JavaModelException {
-        IType mainBuildType = getBuildClassType(javaProject).getType();
-        List<IMethod> methods = new LinkedList<>();
-        for (IMethod method : mainBuildType.getMethods()) {
-            
+        ITypeHierarchy typeHierarchy = getBuildClassType(javaProject);
+        int i = index;
+        List<IMethod> methods = getPublicNoArgMethods(typeHierarchy);
+        Collections.sort(methods, new MethodComparator());
+        for (IMethod method : methods) {
+            MethodDescription methodDescription = new MethodDescription(method);
+            MenuItem menuItem = new MenuItem(menu, SWT.CHECK, i);
+            menuItem.setText(methodDescription.getName());
+            menuItem.addSelectionListener(new SelectionAdapter() {
+                public void widgetSelected(SelectionEvent e) {
+                    System.err.println("Dynamic menu selected");
+                }
+            });
+            menuItem.setToolTipText(methodDescription.getDefinition());
+            index++;
         }
     }
 
@@ -90,7 +101,7 @@ public class LaunchMenu extends ContributionItem {
     private static ITypeHierarchy getBuildClassType(IJavaProject javaProject) throws JavaModelException {
         IType jkBuildType = javaProject.findType("org.jerkar.tool.JkBuild");
         for (IPackageFragmentRoot fragmentRoot : javaProject.getPackageFragmentRoots()) {
-            IPath path = fragmentRoot.getPath();
+            IPath path = fragmentRoot.getPath().makeRelativeTo(javaProject.getPath());
             if (fragmentRoot.getKind() == IPackageFragmentRoot.K_SOURCE && path.equals(DEF_PATH)) {
                 IType selectedType = null;
                 ITypeHierarchy selectedHierarchy = null;
@@ -99,8 +110,12 @@ public class LaunchMenu extends ContributionItem {
                     for (ICompilationUnit compilationUnit : packageFragment.getCompilationUnits()) {
                         IType type = compilationUnit.getTypes()[0];
                         String name = type.getFullyQualifiedName();
-                        boolean lesser = name.compareTo(selectedType.getFullyQualifiedName()) < 0;
-                        if (selectedType == null || lesser) {
+
+                        // We want to get the first compiled class in this
+                        // fragment
+                        boolean lesser = selectedType == null
+                                || name.compareTo(selectedType.getFullyQualifiedName()) < 0;
+                        if (lesser) {
                             ITypeHierarchy typeHierarchy = type.newSupertypeHierarchy(null);
                             for (IType type2 : typeHierarchy.getAllSuperclasses(type)) {
                                 if (type2.equals(jkBuildType)) {
@@ -119,5 +134,48 @@ public class LaunchMenu extends ContributionItem {
         return null;
     }
 
-   
+    private static List<IMethod> getPublicNoArgMethods(ITypeHierarchy typeHierarchy) throws JavaModelException {
+        List<IMethod> result = new LinkedList<>();
+        result.addAll(getPublicNoArgMethods(typeHierarchy.getType()));
+        for (IType type : typeHierarchy.getAllSuperclasses(typeHierarchy.getType())) {
+            if (!type.getFullyQualifiedName().equals(Object.class.getName())) {
+                result.addAll(getPublicNoArgMethods(type));
+            }
+        }
+        return result;
+
+    }
+
+    private static List<IMethod> getPublicNoArgMethods(IType type) throws JavaModelException {
+        List<IMethod> result = new LinkedList<>();
+        for (IMethod method : type.getMethods()) {
+            if (!Flags.isAbstract(method.getFlags()) 
+                    && method.getNumberOfParameters() == 0
+                    && Flags.isPublic(method.getFlags()) 
+                    && method.getReturnType().endsWith(Signature.SIG_VOID)
+                    && !method.isConstructor()
+                    && !method.getElementName().equals("init")) {
+                result.add(method);
+            }
+        }
+        return result;
+    }
+
+    private boolean isJavaProject(IProject project) {
+        try {
+            return project.hasNature("org.eclipse.jdt.core.javanature");
+        } catch (CoreException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    private static class MethodComparator implements Comparator<IMethod> {
+
+        @Override
+        public int compare(IMethod o1, IMethod o2) {
+            return - o1.getElementName().compareTo(o2.getElementName());
+        }
+        
+    }
+
 }
--- org.jerkar.eclipse-plugin/src/org/jerkar/eclipseplugin/menu/MethodDescription.java
+++ org.jerkar.eclipse-plugin/src/org/jerkar/eclipseplugin/menu/MethodDescription.java
@@ -1,7 +1,12 @@
 package org.jerkar.eclipseplugin.menu;
 
+import org.eclipse.jdt.core.IAnnotation;
+import org.eclipse.jdt.core.IMemberValuePair;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.JavaModelException;
+
 public class MethodDescription {
-    
+
     private final String name;
 
     private final String definition;
@@ -11,7 +16,11 @@ public class MethodDescription {
         this.name = name;
         this.definition = definition;
     }
-    
+
+    public MethodDescription(IMethod method) throws JavaModelException {
+        this(method.getElementName(), description(method));
+    }
+
     public String getName() {
         return name;
     }
@@ -19,5 +28,18 @@ public class MethodDescription {
     public String getDefinition() {
         return definition;
     }
-  
+
+    private static String description(IMethod method) throws JavaModelException {
+        IAnnotation annotation = method.getAnnotation("org.jerkar.tool.JkDoc");
+        if (annotation.exists()) {
+            IMemberValuePair[] pairs = annotation.getMemberValuePairs();
+            if (pairs.length == 0) {
+                return null;
+            }
+            Object[] value = (Object[]) pairs[0].getValue(); 
+            return (String) value[0];
+        }
+        return null;
+    }
+
 }
